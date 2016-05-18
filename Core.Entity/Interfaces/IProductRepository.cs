1c19764f143e364302473295f6f4e47bf729ecb3
Anton Vdovin <antonvdovin92@gmail.com>
2016-05-18 19:34:53 +0300

    add uncommited file

---
+++ Core.Entity/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Core.Entity.Models;
+
+namespace Core.Entity.Interfaces
+{
+    /// <summary>
+    /// Represents <see cref="Product"/> repository
+    /// </summary>
+    public interface IProductRepository : IRepository<Product>
+    {
+        /// <summary>
+        /// Get <see cref="Product"/> collection that have most expensive price
+        /// </summary>
+        /// <param name="count">Count return collection</param>
+        /// <returns><see cref="Product"/> collection descending by price</returns>
+        IEnumerable<Product> MostExpensiveProduct(int count);
+    }
+}
