9f494c152a67c99536b01d50bb12befdee402d82
Vdovin Anton <antonvdovin@outlook.com>
2016-05-21 11:39:26 +0300

    show db info in console

--- Core/Startup.cs
+++ Core/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Core.Entity;
 
 namespace Core
@@ -13,9 +14,19 @@ namespace Core
 
                 var orderFrom = unitOfWork.Orders.GetOrdersFromDate(new DateTime(2016, 05, 20, 13, 47, 0));
 
-                var order = unitOfWork.Orders.GetOrderById(1);
+                var order = unitOfWork.Orders.GetOrderById(100);
+
+                var whoMadeOrder = unitOfWork.People.GetPersonWithOrders(order.PersonId);
 
                 var topProducts = unitOfWork.Products.MostExpensiveProduct(10);
+
+                Console.WriteLine($"Persone with id 1: {persone.FirstName} {persone.LastName}");
+                Console.WriteLine("");
+                Console.WriteLine($"Date order with id 1: : {order.DateTime}");
+                Console.WriteLine($"And this order did {whoMadeOrder.FirstName}");
+                Console.WriteLine("");
+                Console.WriteLine("Top 10 buyer products:");
+                topProducts.ToList().ForEach(w => { Console.WriteLine($" - {w.Name}"); });
             }
         }
     }
